8c0a2005199dd1c2be99a8dab10e13f9aa6df1e5
Farbfetzen <farbfetzen@posteo.de>
2023-11-02 22:17:58 +0100

    Make the colors in noised2 brighter

--- sketches/noise2d/sketch.js
+++ sketches/noise2d/sketch.js
@@ -15,7 +15,12 @@ function draw() {
         let xOffset = 0;
         for (let x = 0; x < width; x++) {
             const index = (x + y * width) * 4;
-            pixels[index] = noise(xOffset, yOffset) * 255;
+            let n = noise(xOffset, yOffset) * 255;
+            // The results from noise() are concentrated around 0.5 and rarely go beyond 0.2 or 0.8.
+            // This transforms the value so the colors become more vibrant.
+            n = map(n, 50, 200, 0, 255);
+            n = constrain(n, 0, 255);
+            pixels[index] = n;
             pixels[index + 1] = noise(xOffset + greenOffset, yOffset + greenOffset) * 255;
             pixels[index + 2] = noise(xOffset + blueOffset, yOffset + blueOffset) * 255;
             pixels[index + 3] = 255;
