7e235fe54f20c8e7396e1efa9973acf065b50375
Farbfetzen <farbfetzen@posteo.de>
2023-12-01 00:36:23 +0100

    Add fading, line trimming and other stuff to epicycles

--- sketches/epicycles/sketch.js
+++ sketches/epicycles/sketch.js
@@ -1,7 +1,8 @@
 // TODO: Find out why closed_hilbert loads so slowly and make it faster.
-// TODO: Implement line trimming and fading from the python project.
 
 const filename = "shapes/heart.txt";
+let fadeLine = false;
+let showCircles = true;
 const canvasWidth = 800;
 const canvasHeight = 800;
 // A number > 0 and <= 1 indicating how much of the width and height of the window the shape should occupy.
@@ -12,21 +13,25 @@ const minAngularVelocity = 1 / 32;
 const maxAngularVelocity = 4;
 // Interpolate if points are farther apart than this.
 const maxDistance = 5;
+// Skip point if distance to previous point is smaller than this to avoid creating unnecessary points.
+const minDistance = 2;
 
-let path;
+const path = [];
 let points = [];
-let signal;
+let angles = [];
+const signal = [];
 let time = 0;
 let epicycleOrigin;
 let currentAngle = 0;
 let maxDeltaTime;
+let lineColor;
+let backgroundColor;
 
 function preload() {
     loadStrings(filename, loadPath);
 }
 
 function loadPath(lines) {
-    path = [];
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i];
         if (!line) {
@@ -91,10 +96,9 @@ function transformPath() {
  * X_k = SUM(n=0, N-1)(x_n * e^(-(i * 2 * PI * k * n) / N))
  */
 function dft() {
-    signal = [];
     const x = path.map((p) => math.complex(p.x, p.y));
     const N = path.length;
-    const minusI2Pi = math.i.mul(math.tau).neg();
+    const minusI2Pi = math.i.mul(TWO_PI).neg();
     for (let k = 0; k < N; k++) {
         let X_k = math.complex();
         for (let n = 0; n < N; n++) {
@@ -128,6 +132,8 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     noFill();
     maxDeltaTime = (1 / getTargetFrameRate()) * 1000 * 2;
+    lineColor = color(255, 0, 255);
+    backgroundColor = color(32);
     transformPath();
     dft();
     setOrigin();
@@ -136,35 +142,53 @@ function setup() {
     // Add first point to implify interpolation calculation.
     const p = getEpicyclesAtAngle(currentAngle, false);
     points.push(p);
+    angles.push(currentAngle);
 }
 
 function draw() {
-    background(32);
-
     // Limit dt because deltaTime increases even when isLooping() is false.
     const dt = min(deltaTime, maxDeltaTime) / 1000;
+
+    background(backgroundColor);
 
     const previousAngle = currentAngle;
     currentAngle += angularVelocity * dt;
     const previousPoint = points[points.length - 1];
-    const currentPoint = getEpicyclesAtAngle(currentAngle, true);
-    if (previousPoint.dist(currentPoint) > maxDistance) {
-        const [interpolatedPoints] = interpolate(previousPoint, previousAngle, currentPoint, currentAngle);
+    const currentPoint = getEpicyclesAtAngle(currentAngle, showCircles);
+    const distance = previousPoint.dist(currentPoint);
+    if (distance > maxDistance) {
+        const [interpolatedPoints, interpolatedAngles] = interpolate(
+            previousPoint,
+            previousAngle,
+            currentPoint,
+            currentAngle
+        );
         points.push(...interpolatedPoints);
+        angles.push(...interpolatedAngles);
     }
-    points.push(currentPoint);
+    if (distance > minDistance) {
+        points.push(currentPoint);
+        angles.push(currentAngle);
+    }
 
-    beginShape();
-    stroke(0, 255, 255);
+    trimArrays();
+
     strokeWeight(2);
-    for (const p of points) {
-        vertex(p.x, p.y);
-    }
-    endShape();
-
-    if (currentAngle >= TWO_PI) {
-        currentAngle -= TWO_PI;
-        points = points.slice(-1);
+    if (fadeLine) {
+        for (let i = 0; i < points.length - 1; i++) {
+            const angle = angles[i];
+            const from = points[i];
+            const to = points[i + 1];
+            stroke(lerpColor(lineColor, backgroundColor, (currentAngle - angle) / TWO_PI));
+            line(from.x, from.y, to.x, to.y);
+        }
+    } else {
+        stroke(lineColor);
+        beginShape();
+        for (const p of points) {
+            vertex(p.x, p.y);
+        }
+        endShape();
     }
 }
 
@@ -188,7 +212,7 @@ function interpolate(point1, angle1, point2, angle2) {
     return [resultPoints, resultAngles];
 }
 
-function getEpicyclesAtAngle(angle, drawCircles) {
+function getEpicyclesAtAngle(angle, showCircles) {
     let x = epicycleOrigin.x;
     let y = epicycleOrigin.y;
     strokeWeight(1);
@@ -199,7 +223,7 @@ function getEpicyclesAtAngle(angle, drawCircles) {
         x += s.amplitude * cos(phi);
         y += s.amplitude * sin(phi);
 
-        if (drawCircles && s.amplitude >= 1) {
+        if (showCircles && s.amplitude >= 1) {
             stroke(128);
             ellipse(previousx, previousY, s.amplitude + s.amplitude);
             stroke(255);
@@ -207,6 +231,20 @@ function getEpicyclesAtAngle(angle, drawCircles) {
         }
     }
     return createVector(x, y);
+}
+
+/**
+ * Keep the points and angles arrays short by removing points that are more than TWO_PI radians behind.
+ */
+function trimArrays() {
+    const limit = currentAngle - TWO_PI;
+    for (let i = 0; i < angles.length; i++) {
+        if (angles[i] > limit) {
+            angles = angles.slice(i);
+            points = points.slice(i);
+            break;
+        }
+    }
 }
 
 function keyPressed() {
@@ -220,5 +258,15 @@ function keyPressed() {
         angularVelocity *= 2;
     } else if (key == "-" && angularVelocity > minAngularVelocity) {
         angularVelocity /= 2;
+    } else if (key === "f") {
+        fadeLine = !fadeLine;
+    } else if (key === "c") {
+        showCircles = !showCircles;
+    } else if (key === "d") {
+        // debug info
+        console.log(
+            `currentAngle: ${currentAngle}\npoints: ${points.length}\nangles: ${angles.length}` +
+                `\nspeed: ${angularVelocity}`
+        );
     }
 }
