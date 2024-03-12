5e1278059b07d1a9cd55e5cde219842137ce1d39
Farbfetzen <farbfetzen@posteo.de>
2024-03-12 21:03:36 +0100

    Convert noise2d

--- sketches/noise2d/README.md
+++
@@ -1,3 +0,0 @@
-# Experiments with 2D Noise
-
-Based on the video [I.5: 2D Noise - Perlin Noise and p5.js Tutorial](https://www.youtube.com/watch?v=ikwNrFvnL3g) by [The Coding Train](https://www.youtube.com/@TheCodingTrain).
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from "@angular/router";
 import { CollatzConjectureComponent } from "src/app/sketches/collatz-conjecture/collatz-conjecture.component";
 import { LangtonsAntComponent } from "src/app/sketches/langtons-ant/langtons-ant.component";
 import { Noise1dComponent } from "src/app/sketches/noise1d/noise1d.component";
+import { Noise2dComponent } from "src/app/sketches/noise2d/noise2d.component";
 import { WelcomeComponent } from "src/app/welcome/welcome.component";
 
 export const routes: Routes = [
@@ -13,6 +14,7 @@ export const routes: Routes = [
             { path: "collatz-conjecture", component: CollatzConjectureComponent },
             { path: "langtons-ant", component: LangtonsAntComponent },
             { path: "noise1d", component: Noise1dComponent },
+            { path: "noise2d", component: Noise2dComponent },
         ],
     },
 ];
