b0a4c85e822af46fd7d6c442db4a2f42380b0a61
Farbfetzen <farbfetzen@posteo.de>
2024-03-24 11:03:00 +0100

    Add link to welcome page

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 import { ToolbarService } from "src/app/toolbar/toolbar.service";
 
 @Component({
     selector: "app-toolbar",
     standalone: true,
-    imports: [],
+    imports: [RouterLink],
     templateUrl: "./toolbar.component.html",
 })
 export class ToolbarComponent {
