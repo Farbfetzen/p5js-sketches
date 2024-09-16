6e83b53438e078d39a3437116492f3153e23b063
Farbfetzen <farbfetzen@posteo.de>
2024-09-16 22:33:29 +0200

    Install primeng and primeicons
    and provide animations as described in https://primeng.org/installation#animations.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { TitleStrategy, provideRouter } from "@angular/router";
 
 import { routes } from "src/app/app.routes";
 import { AppTitleStrategy } from "src/app/title-strategy/app-title-strategy";
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), { provide: TitleStrategy, useClass: AppTitleStrategy }],
+    providers: [provideRouter(routes), { provide: TitleStrategy, useClass: AppTitleStrategy }, provideAnimations()],
 };
