a3470e5a98104da98c382a3d4a48556e93a1b298
Farbfetzen <farbfetzen@posteo.de>
2024-09-17 22:02:32 +0200

    Set eventCoalescing to true
    because that is done in fresh angular apps created by the cli.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { TitleStrategy, provideRouter } from "@angular/router";
 
@@ -6,5 +6,10 @@ import { routes } from "src/app/app.routes";
 import { AppTitleStrategy } from "src/app/title-strategy/app-title-strategy";
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), { provide: TitleStrategy, useClass: AppTitleStrategy }, provideAnimations()],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        { provide: TitleStrategy, useClass: AppTitleStrategy },
+        provideAnimations(),
+    ],
 };
