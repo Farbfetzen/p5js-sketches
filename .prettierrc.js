b373e541319ad578f71bc4d83b4efd3031a12b3e
Farbfetzen <farbfetzen@posteo.de>
2024-10-04 23:45:13 +0200

    Install prettier-plugin-organize-attributes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,27 @@
 module.exports = {
-    plugins: ["@trivago/prettier-plugin-sort-imports"],
+    plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-organize-attributes"],
 
     // Plugin: '@trivago/prettier-plugin-sort-imports'
     importOrder: ["^zone.js", "^@angular", "^@|^rxjs", "^src"],
     importOrderCaseInsensitive: true,
     importOrderParserPlugins: ["typescript", "decorators-legacy"],
     importOrderSeparation: true,
+
+    // Plugin: prettier-plugin-organize-attributes
+    // See https://github.com/NiklasPor/prettier-plugin-organize-attributes/blob/main/src/presets.ts
+    attributeGroups: [
+        "^(id|name)$",
+        "$ANGULAR_STRUCTURAL_DIRECTIVE",
+        "$ANGULAR_TWO_WAY_BINDING",
+        "$ANGULAR_ANIMATION",
+        "$ANGULAR_ANIMATION_INPUT",
+        "$ANGULAR_INPUT",
+        "$ANGULAR_OUTPUT",
+        "$ANGULAR_ELEMENT_REF",
+        "^cdk",
+        "^form",
+        "$DEFAULT",
+        "^aria-",
+    ],
+    attributeSort: "ASC",
 };
